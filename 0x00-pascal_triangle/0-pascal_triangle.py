637dc657342715dfec351e6e90e3497ab52eba06
Obed101 <obedamoako92@gmail.com>
2022-04-07 12:53:09 +0000

    created pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal_triangle(n):
+    """ A function that prints a list of
+    lists of integers in pascal way
+    """
+    if n <= 0:
+        return []
+    for line in range(1, n + 1):
+        C = 1
+        for i in range(1, line + 1):
+            print(C, end=" ")
+            C = int(C * (line - i) / i)
+        print()
