53050b953d01f52b31a3c307e77624218c3c7e1b
Obed101 <obedamoako92@gmail.com>
2022-04-07 16:24:54 +0000

    fixed :wrench: pascals triangle output error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,11 +8,12 @@ def pascal_triangle(n):
     """ A function that prints a list of
     lists of integers in pascal way
     """
-    if n <= 0:
-        return []
-    for line in range(1, n + 1):
-        C = 1
-        for i in range(1, line + 1):
-            print(C, end=" ")
-            C = int(C * (line - i) / i)
-        print()
+    triangle = []
+    row = []
+    prev_row = []
+    for i in range(0, n + 1):
+        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1]
+               + prev_row[j] or 1 for j in range(0, i)]
+        prev_row = row
+        triangle += [row]
+    return triangle[1:]
