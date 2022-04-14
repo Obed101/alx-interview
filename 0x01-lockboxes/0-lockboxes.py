1f8ddb50f6c7d4d18635214673aeb6e2353a0051
Obed Amoako <obedamoako92@gmail.com>
2022-04-14 11:12:39 +0000

    removed test cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -34,12 +34,3 @@ def canUnlockAll(boxes):
             return True
         keys = new_keys
     return False
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [5, 6], [5, 6, 2], [3], [4, 1]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
