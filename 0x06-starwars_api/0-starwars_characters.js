1c5d5bfafd319a79214ed7f126426ddb4fee99d8
Obed Amoako <obedamoako92@gmail.com>
2022-05-20 16:49:02 +0000

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,13 +4,12 @@ movie_id = process.argv[2];
 
 const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 request(url, (error, response, body) => {
-  if (error) { return 'There Was An Error One   '; }
+  if (error) { return `There Was An Error In First Request`; }
   if (response.statusCode === 200) {
     body = JSON.parse(body);
-    // body.characters.sort()
     for (chars of body.characters) {
       request(chars, (error, response, body) => {
-        if (error) {return 'There Was An Error'; }
+        if (error) {return `Could Not Get This Chararacter`; }
         if (response.statusCode === 200) {
           body = JSON.parse(body);
           console.log(body.name);
