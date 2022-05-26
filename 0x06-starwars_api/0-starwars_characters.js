fdfcc354d4ac87cfc7b5f62bdcada9c81c663485
Obed Amoako <obedamoako92@gmail.com>
2022-05-26 12:55:18 +0000

    fixed output problem

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,25 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const request = require('request');
 movie_id = process.argv[2];
 
+const char_list = [];
 const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 request(url, (error, response, body) => {
-  if (error) return    // In case the url is unreachable
+  if (error) return console.log('Cannot connect'); // In case it is unreachable
   if (response.statusCode === 200) {
     body = JSON.parse(body);
     for (chars of body.characters) {
-      request(chars, (error, response, body) => {
-        if (error) return
-        if (response.statusCode === 200) {
-          body = JSON.parse(body);
-          console.log(body.name);
-        }
-      });
+      item = function () {
+        request(chars, (error, _, res) => {
+          if (error) return console.log('error fetching name');
+          cha =  JSON.parse(res).name;
+          console.log(cha)
+        });
+      };
+      char_list.push(item());
     }
+    // for (char of char_list) {
+    //   console.log(char)
+    // }
   }
 });
