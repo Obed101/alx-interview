5f56462c0add589f9ee11b0461a141b678a5bd81
Obed Amoako <obedamoako92@gmail.com>
2022-05-26 08:15:07 +0000

    changed file mode

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ movie_id = process.argv[2];
 
 const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 request(url, (error, response, body) => {
-  if (error) return
+  if (error) return    // In case the url is unreachable
   if (response.statusCode === 200) {
     body = JSON.parse(body);
     for (chars of body.characters) {
