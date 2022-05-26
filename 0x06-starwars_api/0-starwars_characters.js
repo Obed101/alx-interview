297b4a4996f49354ec3daf7654782a7fd9d82678
Obed Amoako <obedamoako92@gmail.com>
2022-05-26 13:41:23 +0000

    changed response

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,21 +5,18 @@ movie_id = process.argv[2];
 const char_list = [];
 const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 request(url, (error, response, body) => {
-  if (error) return console.log('Cannot connect'); // In case it is unreachable
+  if (error) return console.log('Cannot connect');
   if (response.statusCode === 200) {
     body = JSON.parse(body);
     for (chars of body.characters) {
       item = function () {
         request(chars, (error, _, res) => {
-          if (error) return console.log('error fetching name');
-          cha =  JSON.parse(res).name;
-          console.log(cha)
+          if (error) return console.log('Cannot get actors name');
+          actor =  JSON.parse(res).name;
+          console.log(actor)
         });
       };
       char_list.push(item());
     }
-    // for (char of char_list) {
-    //   console.log(char)
-    // }
   }
 });
