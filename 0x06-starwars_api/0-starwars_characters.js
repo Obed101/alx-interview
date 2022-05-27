0accd5589b4bce075c9801b09439890bd35437b4
Obed Amoako <obedamoako92@gmail.com>
2022-05-27 10:44:39 +0000

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-// Takes movie Id as argument and returns 
+// Takes movie Id as argument and returns
 // returns all character's names to the console
 // in specific order
 
 const request = require('request');
-const movie_id = process.argv[2];
-const char_list = [];
-const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
+const movieId = process.argv[2];
+const charList = [];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, async function(error, _, body) {
+request(url, async function (error, _, body) {
   if (error) return `Unable to connect`;
   body = JSON.parse(body);
 
-  for (chars of body.characters) {
-
-    item = new Promise(function (resolve) {
+  for (const chars of body.characters) {
+    const item = new Promise(function (resolve) {
       request(chars, (error, _, res) => {
         if (error) return `Cannot get Actor's name`;
         resolve(JSON.parse(res).name);
@@ -25,6 +24,6 @@ request(url, async function(error, _, body) {
   const actors = await Promise.all(char_list);
   // finally printing the character names
   for (const actor of actors) {
-    console.log(actor);
+    console.log(actor)
   }
 });
