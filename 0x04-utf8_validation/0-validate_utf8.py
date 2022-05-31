0232f6a3a4a32b4ef27271d0d81d3d5d8fbdb500
Obed Amoako <obedamoako92@gmail.com>
2022-05-31 19:51:36 +0000

    fixed boolean output

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,8 @@
 #!/usr/bin/python3
 
 """
-validate utf-8
+Checks if data is valid utf-8
 """
-
-
-def validUTF8_v1(data):
-    """
-    Checks if data is valid utf-8
-    """
-    count = 0
-
-    if not data:
-        return False
-
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
-
-        if count == 0:
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                count += 1
-
-            if count == 0:
-                continue
-
-            if count == 1 or count > 4:
-                return False
-        else:
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                return False
-    return count == 0
 
 
 def validUTF8(data):
@@ -60,30 +31,3 @@ def validUTF8(data):
     if count == 0:
         return True
     return False
-
-
-def validUTF8_v4(data):
-    n_bytes = 0
-
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
-    if data == [240, 188, 128, 167]:
-        return False
-    for num in data:
-        mask = 1 << 7
-        if n_bytes == 0:
-            while mask & num:
-                n_bytes += 1
-                mask = mask >> 1
-
-            if n_bytes == 0:
-                continue
-
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-        else:
-            if not (num & mask1 and not (num & mask2)):
-                return False
-        n_bytes -= 1
-        return n_bytes == 0
