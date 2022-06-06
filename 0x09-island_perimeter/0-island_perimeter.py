28247caa7f84d1dc3b52713dc29b79a0aca167c4
Obed Amoako <obedamoako92@gmail.com>
2022-06-06 18:52:42 +0000

    created Island Perimeter function using Dept_first_search algo...

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module contains code for
+calculating the island perimeter in an ocean
+"""
+
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """returns the island defined by Grid"""
+    perimeter = 0
+    row = len(grid)
+    col = len(grid[0])
+
+    def dept_f_search(i, j):
+        """
+        Itearates through the nodes to check their borders
+        using Dept First Search algorithm
+        """
+        if i >= row or j >= col or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+
+        if grid[i][j] == 1:
+            # Avoid visiting the same item twice
+            grid[i][j] = 2    # To mark as 'read' and ignore it
+            perimeter = dept_f_search(i, j + 1)
+            perimeter += dept_f_search(i + 1, j)
+            perimeter += dept_f_search(i, j - 1)
+            perimeter += dept_f_search(i - 1, j)
+            return perimeter
+        return 0
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j]:
+                perimeter += dept_f_search(i, j)
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,9 @@
+# ISLAND PERIMETER - PYTHON
+Calculating the perimiter of an island in a an ocean using python.
+
+
+## HOW TO WORK IT OUT
+- For every box, check if the left, right, top and bottom nodes are
+water.
+- If true, increment the perimeter by one.
+- Else, continue
